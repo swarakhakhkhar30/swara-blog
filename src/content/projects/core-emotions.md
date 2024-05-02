b449e2264947dd59ef07fb715a942d754edbc9ea
swarakhakhkhar30 <swarakhakhkhar30@gmail.com>
2024-05-02 18:44:01 +0530

    started editing

---
+++ src/content/projects/core-emotions.md
@@ -0,0 +1,13 @@
+---
+title: Core Emotions
+description: A digital exhibit exploring the complex interplay of emotions and their role in our daily life
+publishDate: 'May 2,2024'
+isFeatured: true
+seo:
+  image:
+    src: 'project-7.jpg'
+---
+
+## The idea
+
+Core Emotions is a journey into the trippy world of human emotions and personality. You'll explore where feelings like joy, sadness, and anger come from , how they influence the things we do and how they influence our personality. As you wander through, you'll gain some insights into how life events shape who you are over time. 
--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,8 +38,8 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: 'Swara Khakhkhar',
+    subtitle: 'Psychologist in making',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
@@ -83,25 +83,21 @@ const siteConfig: SiteConfig = {
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
+            text: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/swara-khakhkhar-195565260/'
+        },
+        {
+            text: 'Mail',
+            href: 'mailto:swara@swarakhakhkhar.com'
         },
         {
             text: 'Instagram',
-            href: 'https://instagram.com/'
-        },
-        {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
+            href: 'https://www.instagram.com/_swara2211'
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-        image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
-        },
+        title: 'Hi there! Good to see you here; welcome to my space!',
+        text: "I'm **Swara Khakhkhar**, an undergraduate student at Ahmedabad University. I aspire to become a clinical psychologist. dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         actions: [
             {
                 text: 'Get in Touch',
